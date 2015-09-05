450d07a6a7938631d302f78950b6743935775b32
ARCANEDEV <arcanedev.maroc@gmail.com>
2015-09-05 03:15:28 +0100

    Adding User model

---
+++ app/Models/User.php
@@ -0,0 +1,45 @@
+<?php namespace App\Models;
+
+use App\Bases\Model;
+use Illuminate\Auth\Authenticatable;
+use Illuminate\Auth\Passwords\CanResetPassword;
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
+use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
+
+/**
+ * Class User
+ * @package App\Models
+ */
+class User extends Model implements AuthenticatableContract, CanResetPasswordContract
+{
+    /* ------------------------------------------------------------------------------------------------
+     |  Traits
+     | ------------------------------------------------------------------------------------------------
+     */
+    use Authenticatable, CanResetPassword;
+
+    /* ------------------------------------------------------------------------------------------------
+     |  Properties
+     | ------------------------------------------------------------------------------------------------
+     */
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'users';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['name', 'email', 'password'];
+
+    /**
+     * The attributes excluded from the model's JSON form.
+     *
+     * @var array
+     */
+    protected $hidden = ['password', 'remember_token'];
+}
