d7906744414dd034cdb97d4f97619fff57fb5a8e
ARCANEDEV <arcanedev.maroc@gmail.com>
2015-09-18 09:15:47 +0100

    Updating gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,52 @@
-var elixir = require('laravel-elixir');
+var gulp   = require('gulp'),
+    elixir = require('laravel-elixir');
+
+/* --------------------------------------------------------------------------
+ *  General Configs
+ * --------------------------------------------------------------------------
+ */
+var bowerDirectory        = './vendor/bower_components/',
+    publicDirectory       = './www/assets/';
+
+/* --------------------------------------------------------------------------
+ *  Elixir Configs
+ * --------------------------------------------------------------------------
+ */
+elixir.config.assetsDir   = 'resources/assets/';
+elixir.config.cssOutput   = publicDirectory + 'css';
+elixir.config.jsOutput    = publicDirectory + 'js';
+elixir.config.bowerDir    = bowerDirectory;
+elixir.config.sourcemaps  = false;
+//elixir.config.production  = !! util.env.production;
+//elixir.config.srcDir      = 'app';
+//elixir.config.tasks       = [];
+//elixir.config.watchers    = { default: {} };
+//elixir.config.duplicate   = [];
+//elixir.config.concatenate = {
+//    css: [],
+//    js: []
+//};
 
 /* --------------------------------------------------------------------------
  *  Elixir Asset Management
  * --------------------------------------------------------------------------
  */
 elixir(function(mix) {
-    mix.less('app.less');
+    // Publishing the fonts
+    mix.copy([
+        bowerDirectory + 'bootstrap/fonts',
+        bowerDirectory + 'font-awesome/fonts'
+    ], publicDirectory + 'fonts');
+
+    // Compiling app scripts
+    mix.scripts([
+        bowerDirectory + 'jquery/dist/jquery.js',
+        bowerDirectory + 'vue/dist/vue.js',
+        bowerDirectory + 'bootstrap/dist/js/bootstrap.js'
+    ], publicDirectory + 'js/vendors.js');
+
+    mix.scripts('app.js', publicDirectory + 'js/app.js');
+
+    // Compiling app styles
+    mix.less('style.less', publicDirectory + 'css/style.css');
 });
