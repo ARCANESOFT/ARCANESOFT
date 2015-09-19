8383c1793919f243d85906ab72421103845f5cd9
ARCANEDEV <arcanedev.maroc@gmail.com>
2015-09-19 13:48:23 +0100

    Updating the resources

---
+++ resources/views/_demo/home.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>ARCANESOFT</title>
+    <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
+</head>
+<body class="arcanesoft">
+    <div class="container">
+        <div class="content">
+            <h1 class="title">ARCANESOFT</h1>
+            <h2>Created by ARCANEDEV <sup class="small">&copy;</sup></h2>
+        </div>
+    </div>
+</body>
+</html>
