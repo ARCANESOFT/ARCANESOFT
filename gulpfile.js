afe3d5527e1b6a203b345fc52774d029779c3de2
ARCANEDEV <arcanedev.maroc@gmail.com>
2015-09-29 19:52:17 +0100

    Removing laravel elixir

--- gulpfile.js
+++ gulpfile.js
@@ -1,52 +1,59 @@
-var gulp   = require('gulp'),
-    elixir = require('laravel-elixir');
-
 /* --------------------------------------------------------------------------
- *  General Configs
+ *  Gulp libs
  * --------------------------------------------------------------------------
  */
-var bowerDirectory        = './vendor/bower_components/',
-    publicDirectory       = './www/assets/';
+var gulp      = require('gulp'),
+    less      = require('gulp-less'),
+    concat    = require('gulp-concat'),
+    minifyCss = require('gulp-minify-css'),
+    uglify    = require('gulp-uglify');
 
 /* --------------------------------------------------------------------------
- *  Elixir Configs
+ *  Directories
  * --------------------------------------------------------------------------
  */
-elixir.config.assetsDir   = 'resources/assets/';
-elixir.config.cssOutput   = publicDirectory + 'css';
-elixir.config.jsOutput    = publicDirectory + 'js';
-elixir.config.bowerDir    = bowerDirectory;
-elixir.config.sourcemaps  = false;
-//elixir.config.production  = !! util.env.production;
-//elixir.config.srcDir      = 'app';
-//elixir.config.tasks       = [];
-//elixir.config.watchers    = { default: {} };
-//elixir.config.duplicate   = [];
-//elixir.config.concatenate = {
-//    css: [],
-//    js: []
-//};
+var resourcesDirectory    = './resources/assets',
+    publicDirectory       = './www',
+    bowerDirectory        = resourcesDirectory + '/bower',
+    assetsDirectory       = publicDirectory + '/assets';
 
 /* --------------------------------------------------------------------------
- *  Elixir Asset Management
+ *  Tasks
  * --------------------------------------------------------------------------
  */
-elixir(function(mix) {
-    // Publishing the fonts
-    mix.copy([
-        bowerDirectory + 'bootstrap/fonts',
-        bowerDirectory + 'font-awesome/fonts'
-    ], publicDirectory + 'fonts');
+gulp.task('default', ['less', 'js']);
 
-    // Compiling app scripts
-    mix.scripts([
-        bowerDirectory + 'jquery/dist/jquery.js',
-        bowerDirectory + 'vue/dist/vue.js',
-        bowerDirectory + 'bootstrap/dist/js/bootstrap.js'
-    ], publicDirectory + 'js/vendors.js');
+gulp.task('all', ['fonts', 'js-vendors', 'default']);
 
-    mix.scripts('app.js', publicDirectory + 'js/app.js');
+gulp.task('less', function () {
+    gulp.src(resourcesDirectory + '/less/style.less')
+        .pipe(less())
+        .pipe(minifyCss({
+            keepSpecialComments: 0
+        }))
+        .pipe(gulp.dest(assetsDirectory + '/css/'));
+});
 
-    // Compiling app styles
-    mix.less('style.less', publicDirectory + 'css/style.css');
+gulp.task('js', function () {
+    gulp.src(resourcesDirectory + '/js/app.js')
+        .pipe(uglify())
+        .pipe(gulp.dest(assetsDirectory + '/js/'));
+});
+
+gulp.task('js-vendors', function() {
+    return gulp.src([
+        bowerDirectory + '/jquery/dist/jquery.js',
+        bowerDirectory + '/vue/dist/vue.js',
+        bowerDirectory + '/bootstrap/dist/js/bootstrap.js'
+    ])
+        .pipe(concat('vendors.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest(assetsDirectory + '/js/'));
+});
+
+gulp.task('fonts', function() {
+    gulp.src([
+        bowerDirectory + '/bootstrap/fonts/*',
+        bowerDirectory + '/font-awesome/fonts/*'
+    ]).pipe(gulp.dest(assetsDirectory + '/fonts/'))
 });
