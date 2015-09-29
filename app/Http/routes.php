65c9b107879e134bd43e59da86fd2ef8d6578086
ARCANEDEV <arcanedev.maroc@gmail.com>
2015-09-29 19:53:04 +0100

    Cleaning stuff

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -4,21 +4,23 @@
  |  Application Routes
  | ------------------------------------------------------------------------------------------------
  */
-Route::localizedGroup(function () {
-    Route::get('/', [
-        'as'    => 'public::home',
-        'uses'  => 'PagesController@index'
-    ]);
-
-    Route::get('contact', [
-        'as'    => 'public::contact.get',
-        'uses'  => 'ContactController@getForm'
-    ]);
-
-    Route::post('contact', [
-        'as'    => 'public::contact.send',
-        'uses'  => 'ContactController@sendForm'
-    ]);
+Route::get('/', function() {
+    return view('demo');
 });
 
-include __DIR__ . '/demo-routes.php';
+//Route::localizedGroup(function () {
+//    Route::get('/', [
+//        'as'    => 'public::home',
+//        'uses'  => 'PagesController@index'
+//    ]);
+//
+//    Route::get('contact', [
+//        'as'    => 'public::contact.get',
+//        'uses'  => 'ContactController@getForm'
+//    ]);
+//
+//    Route::post('contact', [
+//        'as'    => 'public::contact.send',
+//        'uses'  => 'ContactController@sendForm'
+//    ]);
+//});
--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -13,14 +13,6 @@ class ExampleTest extends TestCase
     /** @test */
     public function it_can_see_the_home_page()
     {
-        $this->visit('demo')->see('ARCANESOFT');
-    }
-
-    /** @test */
-    public function it_must_have_a_home_route()
-    {
-        $response = $this->route('GET', 'public::home');
-
-        $this->assertEquals(301, $response->status());
+        $this->visit('/')->see('ARCANESOFT');
     }
 }
