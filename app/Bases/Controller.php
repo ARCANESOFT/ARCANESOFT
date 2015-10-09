68309e2ef751309b23cbf0d464359da28669ec3c
ARCANEDEV <arcanedev.maroc@gmail.com>
2015-10-09 17:17:38 +0100

    Updating the Base Classes

--- app/Bases/Controller.php
+++ app/Bases/Controller.php
@@ -1,6 +1,10 @@
 <?php namespace App\Bases;
 
 use Arcanedev\Support\Bases\Controller as BaseController;
+use Arcanedev\Support\Traits\Templatable;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Foundation\Validation\ValidatesRequests;
 
 /**
  * Class     Controller
@@ -10,5 +14,23 @@ use Arcanedev\Support\Bases\Controller as BaseController;
  */
 abstract class Controller extends BaseController
 {
-    //
+    /* ------------------------------------------------------------------------------------------------
+     |  Traits
+     | ------------------------------------------------------------------------------------------------
+     */
+    use AuthorizesRequests, DispatchesJobs, Templatable, ValidatesRequests;
+
+    /* ------------------------------------------------------------------------------------------------
+     |  Constructor
+     | ------------------------------------------------------------------------------------------------
+     */
+    /**
+     * {@inheritdoc}
+     */
+    public function __construct()
+    {
+        parent::__construct();
+
+        $this->setTemplate(config('cms.template'));
+    }
 }
