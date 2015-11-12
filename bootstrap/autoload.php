b1b34a0c4752487abd61d0c9344c6bad25cd500c
ARCANEDEV <arcanedev.maroc@gmail.com>
2015-11-12 20:26:19 +0000

    Adding a comment for the Maximum function nesting level error

--- bootstrap/autoload.php
+++ bootstrap/autoload.php
@@ -1,6 +1,11 @@
 <?php
 
-ini_set('xdebug.max_nesting_level', 120);
+/**
+ * To fix the `Maximum function nesting level of '100' reached, aborting!`
+ *   uncomment the line below
+ *   Or add to your php.ini file this line `xdebug.max_nesting_level = 500`
+ */
+// ini_set('xdebug.max_nesting_level', 500);
 
 define('LARAVEL_START', microtime(true));
 
