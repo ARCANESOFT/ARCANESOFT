b2e2ffa38eecde62a331e84625c74b90117df6de
ARCANEDEV <arcanedev.maroc@gmail.com>
2016-11-15 08:59:26 +0000

    Updating the database config file

--- config/database.php
+++ config/database.php
@@ -28,8 +28,8 @@ return [
 
         'mysql'  => [
             'driver'    => 'mysql',
-            'host'      => env('DB_HOST', 'localhost'),
-            'port'      => env('DB_PORT', '3306'),
+            'host'      => env('DB_HOST',     '127.0.0.1'),
+            'port'      => env('DB_PORT',     '3306'),
             'database'  => env('DB_DATABASE', 'forge'),
             'username'  => env('DB_USERNAME', 'forge'),
             'password'  => env('DB_PASSWORD', ''),
@@ -42,8 +42,8 @@ return [
 
         'pgsql'  => [
             'driver'   => 'pgsql',
-            'host'     => env('DB_HOST', 'localhost'),
-            'port'     => env('DB_PORT', '5432'),
+            'host'     => env('DB_HOST',     '127.0.0.1'),
+            'port'     => env('DB_PORT',     '5432'),
             'database' => env('DB_DATABASE', 'forge'),
             'username' => env('DB_USERNAME', 'forge'),
             'password' => env('DB_PASSWORD', ''),
